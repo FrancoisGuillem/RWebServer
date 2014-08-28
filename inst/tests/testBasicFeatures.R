c282601a2f40c05a4b30c44fd527106e2a66196b
Administrator <admin@example.com>
2014-08-28 11:25:30 +0200

    Added some tests

--- inst/tests/testBasicFeatures.R
+++ inst/tests/testBasicFeatures.R
@@ -6,8 +6,17 @@ context("Basic features")
 test_that("Creation of server works", {
   webServer.skeleton()
   
-  cat("run <- function(x,y) as.numeric(x) + as.numeric(y)", 
+  test <- "hello world"
+  save(test, file="myRWebServer/data/test.rda")
+  
+  cat("myFun <- sum", file="myRWebServer/lib/myFun.R")
+  
+  cat("run <- function(x,y, ...) as.numeric(x) + as.numeric(y)", 
       file="myRWebServer/R/test.R")
+  cat("run <- function(...) test", 
+      file="myRWebServer/R/test2.R")
+  cat("run <- function(x, y, ...) myFun(as.numeric(x), as.numeric(y))", 
+      file="myRWebServer/R/test3.R")
   
   cat("/test/{x}/{y} /test\n", file="myRWebServer/routes")
   
@@ -18,6 +27,16 @@ test_that("Creation of server works", {
 test_that("Server is running", {
   res <- GET("http://localhost:8080/test?x=1&y=2")
   expect_equal(content(res, "text"), "3")
+})
+
+test_that("Data files have been loaded", {
+  res <- GET("http://localhost:8080/test2")
+  expect_equal(content(res, "text"), "hello world")
+})
+
+test_that("R files have been sourced", {
+  res <- GET("http://localhost:8080/test3?x=1&y=1")
+  expect_equal(content(res, "text"), "2")
 })
 
 test_that("Routes work", {
